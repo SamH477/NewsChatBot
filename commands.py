5bf431215f9264a68c2f9b616d8aea278c0cc0a2
SamH477 <huhns@etown.edu>
2023-09-23 14:41:14 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
 #TODO create commands for the chat bot
 
+commands = ["hello", "what's the date?", "what time is it?"]
 
+responses = {"hello": "Hi! How can I help you?", "what's the date?": "Today is "}
