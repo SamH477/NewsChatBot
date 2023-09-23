cfd482f4c815abb728d7f8b7a91cc40f0a4f807d
SamH477 <huhns@etown.edu>
2023-09-23 15:34:36 -0400

    fix commands.py

--- commands.py
+++ commands.py
@@ -3,12 +3,17 @@ import speech_rec
 from datetime import date, datetime
 today = date.today()
 time = datetime.now()
+str_today = today.strftime("%Y-%m-%d")
+str_time = time.strftime("%I:%M%p")
 
-commands = ["hello", "what's the date?", "what time is it?"]
+commands = ["hello", "what's the date", "what time is it"]
 
-responses = {"hello": "Hi! How can I help you?", "what's the date?": today, "what time is it": time}
+responses = {"hello": "Hi! How can I help you?", "what's the date": str_today, "what time is it": str_time}
 
 while True:
     command = speech_rec.voice_recognition()
-    if (command == 'goodbye'):
-        break
+    for cmds in commands:
+        if (cmds == command):
+            print("chatbot: " + responses[cmds])
+        if (command == 'goodbye'):
+            break
