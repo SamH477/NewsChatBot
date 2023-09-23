2c7ecbdbdeb0aeb2b7ecc8d87c707c7b00004537
SamH477 <huhns@etown.edu>
2023-09-23 14:51:49 -0400

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,5 +1,8 @@
 #TODO create commands for the chat bot
+from datetime import date, datetime
+today = date.today()
+time = datetime.now()
 
 commands = ["hello", "what's the date?", "what time is it?"]
 
-responses = {"hello": "Hi! How can I help you?", "what's the date?": "Today is "}
+responses = {"hello": "Hi! How can I help you?", "what's the date?": today, "what time is it": time}
