1d6ca66aff9909f05dcbcf2e2b312e0d06463b05
SamH477 <huhns@etown.edu>
2023-09-23 17:54:10 -0400

    update commands file

--- commands.py
+++ commands.py
@@ -19,10 +19,16 @@ while True:
     for cmds in commands:
         if (cmds == command):
             print("chatbot: " + responses[cmds])
-            if (command == "what's the news"): 
-                #add api key
-                url = f"https://api.mediastack.com/v1/news?access_key=&limit=5"
-                response = requests.get(url)
+        elif (cmds == "what's the news"): 
+            # Add your API key
+            api_key = "YOUR_API_KEY_HERE"
+            url = f"http://api.mediastack.com/v1/news?access_key=&limit=5"
+            response = requests.get(url)
+
+            # Debugging: Print the entire API response
+            print(response.text)
+
+            if response.status_code == 200:
                 data = response.json()
                 if "data" in data:
                     news_articles = data["data"]
@@ -32,8 +38,19 @@ while True:
                             title = article.get("title", "No Title")
                             source = article.get("source", "Unknown Source")
                             news_info += f"- {title} from {source}\n"
-                    print("chatbot: " + news_info)
+                        print("chatbot: " + news_info.encode('utf-8', 'ignore').decode('utf-8'))
+                    else:
+                        print("chatbot: No news articles found.")
                 else:
-                    print("chatbot: No news articles found.")
+                    print("chatbot: 'data' field not found in the API response.")
+            else:
+                print(f"chatbot: API request failed with status code {response.status_code}")
+
         if (command == 'goodbye'):
             break
+
+
+
+
+
+
