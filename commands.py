01d4e1373c99d86783d00289b1d8c81b3395d523
SamH477 <huhns@etown.edu>
2023-09-30 14:15:55 -0400

    update file params

--- commands.py
+++ commands.py
@@ -8,9 +8,10 @@ import pycountry
 sys.stdout.reconfigure(encoding='utf-8')
 
 #date and time commands
+
 today = date.today()
 time = datetime.now()
-str_today = today.strftime("%Y-%m-%d")
+str_today = today.strftime("%Y-%m-%d") 
 str_time = time.strftime("%I:%M%p")
 
 commands = ["hello", "what's the date", "what time is it"]
