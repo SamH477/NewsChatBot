ce0da050ff661694886618e49c75024646e94847
SamH477 <huhns@etown.edu>
2023-10-03 18:48:48 -0400

    take out api key

--- api_config.py
+++ api_config.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace 'YOUR_API_KEY' with your actual API key
-api_key = '3102dede7401422dab6c17ee6ace14af'
+api_key = ''
 
 # Define the base URL for the API
 base_url = 'http://api.mediastack.com/v1/'
